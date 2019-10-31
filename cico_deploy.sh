91b04238be27f5dd702b22d8fb13a6f510e68e23
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-10-31 10:12:06 -0400

    misc

--- cico_deploy.sh
+++ cico_deploy.sh
@@ -2,6 +2,9 @@
 yum -y install docker 
 service docker start
 
+K=AKIAIOSFODNN7EXAMPLE
+S=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
 cd grafana-hawkular
 docker build -t registry.devshift.net/perf/osd-monitor:latest . 
 if [ $? -eq 0 ]; then
